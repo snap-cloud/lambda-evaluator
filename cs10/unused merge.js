be0a6cf81593e34f666bdefd0b43e2cd61f6c957
Michael Ball <cycomachead@gmail.com>
2016-03-02 04:27:43 -0600

    add old code

--- cs10/unused merge.js
+++ cs10/unused merge.js
@@ -6,7 +6,7 @@
     function blockHas2Calls() {
         return occurancesOfBlockSpec(blockSpec, getBlockBody(blockSpec)) == 2;
     }
-    
+
 
     // tip_2_1.newAssertTest(
     //     blockHas2Calls,
@@ -16,18 +16,18 @@
     //     0 // NOTE: Currently no points in case this doesnt work.
     // );
 
-    
+
     // Generate some random data for each test.
     var list1 = [];
     for (var i = 0; i < 10; i += 1) {
         list1.push(Process.prototype.reportRandom(1, 20));
     }
     var result = list1.sort(function (x, y) {return x - y});
-    
+
     var sortBlock = "sort %";
     blockImg = AG_UTIL.HTMLFormattedBlock(sortBlock);
     chunk_2 = fb.newChunk('Let\'s test merge using ' + blockImg + '.');
-    
+
     var tip_2_1 = chunk_1.newTip(
         'Make sure you name your block exactly ' + blockImg + ' and place it in the scripting area.',
         'Found the ' + blockImg + ' block in the scripting area.'
@@ -36,7 +36,7 @@
     var blockExists_2 = function () {
         return spriteContainsBlock(sortBlock);
     }
-    
+
     tip_2_1.newAssertTest(
         blockExists_2,
         "Testing if the " + blockImg + " block is in the scripting area.",
@@ -44,12 +44,12 @@
         "Make sure you name your block exactly " + blockImg + " and place it in the scripting area.",
         0
     );
-    
+
     var tip_2_2 = chunk_2.newTip(
         'Try merging larger lists.',
         'Awesome work! You\'ve sorted a list of 10 items.'
     );
-    
+
     tip_2_2.newIOTest('r',  // testClass
         sortBlock,          // blockSpec
         list1 ,             // input
@@ -62,4 +62,3 @@
         true,               // is isolated
         1                   // points
     );
-    
