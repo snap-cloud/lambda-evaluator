59cf8bf57a61278c3f51a92a91a76194a68e6906
Michael Ball <cycomachead@gmail.com>
2016-03-28 01:54:57 -0700

    resolve merge

--- ag-files/feedbackLog.js
+++ ag-files/feedbackLog.js
@@ -611,12 +611,8 @@ function AssertTest(statement, text, pos_fb, neg_fb, points) {
     this.text = text;
     this.pos_fb = pos_fb;
     this.neg_fb = neg_fb;
-    if (points !== undefined) {
-        this.points = points;
-    } else {
-        this.points = 1;
-    }
-    //this.points = points !== undefined ? points : 1 <<< FIXED ;
+    this.points = points !== undefined ? points : 1;
+
     try {
         this.correct = statement();
         if (this.correct) {
