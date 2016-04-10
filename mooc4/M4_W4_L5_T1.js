21d8ee8cd6321757bd8da00c6faf5172368ce75a
Tina Huang <thuang@berkeley.edu>
2016-04-10 15:36:03 -0700

    changed name slightly

--- mooc4/M4_W4_L5_T1.js
+++ mooc4/M4_W4_L5_T1.js
@@ -6,7 +6,7 @@ var courseID = "BJC.4x";  // e.g. "BJCx"
 var preReqTaskID = null;
 var preReqID = courseID + preReqTaskID;
 // taskID uniquely identifies the task for saving in browser sessionStorage.
-var taskID = "_M4_W4_L3_T1";
+var taskID = "_M4_W4_L5_T1";
 var id = courseID + taskID;
 var isEDX = isEDXurl();
 // if this question is not meant to be graded, change this flag to false
