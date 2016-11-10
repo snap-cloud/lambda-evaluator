c9ea7dc69d68f233473d5c7971e89b61f2c4780d
NathanielLow <low.nathaniel@berkeley.edu>
2016-11-09 23:53:06 -0800

    AG for U7_L3_P4 If There is Time... Exercise, and taskId update for U7_L2_P3_EC1

--- edc/unit7/U7_L3_P4_EC1.js
+++ edc/unit7/U7_L3_P4_EC1.js
@@ -6,7 +6,7 @@ var courseID = "edc";  // e.g. "BJCx"
 var preReqTaskID = null;
 var preReqID = courseID + preReqTaskID;
 // taskID uniquely identifies the task for saving in browser sessionStorage.
-var taskID = "_U7_L2_P3_EC1";
+var taskID = "_U7_L3_P4_EC1";
 var id = courseID + taskID;
 var isEDX = isEDXurl();
 // if this question is not meant to be graded, change this flag to false
@@ -19,12 +19,11 @@ function AGTest(outputLog) {
     var fb = new FeedbackLog(
         world,
         id,
-        'Sorting a List of 500 Elements'
+        'Base'
     );
 
-    var sort = "sort %";
-    var selection = "selection sort %";
-    var partition = "partition sort %";
+    var base = "% base %";
+    var from_base = "% from base %";
 
     /*var spriteIndex;
     var ide = world.children[0];
@@ -36,20 +35,20 @@ function AGTest(outputLog) {
         }
     }*/
 
-    var chunk_1 = fb.newChunk('Complete the "' + sort + '" block.');
+    var chunk_1 = fb.newChunk('Complete the "' + base + '" block.');
 
     var blockExists_1 = function () {
-        return spriteContainsBlock(sort);
+        return spriteContainsBlock(base);
     }
 
-    var tip_1_1 = chunk_1.newTip('Make sure you name your block exactly "' + sort + '", place it in the scripting area, and that it is recursive.',
-        'The "' + sort + '" block exists and is recursive.');
+    var tip_1_1 = chunk_1.newTip('Make sure you name your block exactly "' + base + '", place it in the scripting area, and that it is recursive.',
+        'The "' + base + '" block exists and is recursive.');
 
     tip_1_1.newAssertTest(
         blockExists_1,
-        'Testing if the "' + sort + '" block is in the scripting area.',
-        'The "' + sort + '" block is in the scripting area.',
-        'Make sure you name your block exactly "' + sort + '" and place it in the scripting area.',
+        'Testing if the "' + base + '" block is in the scripting area.',
+        'The "' + base + '" block is in the scripting area.',
+        'Make sure you name your block exactly "' + base + '" and place it in the scripting area.',
         1
     );
 
@@ -58,19 +57,10 @@ function AGTest(outputLog) {
         'Great job! Your block reports the correct value for given inputs.'
     );
 
-    var input = [];
-    for (var i = 0; i < 500; i++) {
-        input.push(getRandomArbitrary(0, 500));
-    }
-    out = [];
-    for (var j = 0; j < input.length; j++) {
-        out.push(input[j]);
-    }
-    out.sort();
-    out.map(String);
-    var input_1_2_1 = [input];
+    
+    var input_1_2_1 = ["1234567", "16"];
     tip_1_2.newIOTest('r',  // testClass
-        sort,          // blockSpec
+        base,          // blockSpec
         input_1_2_1,        // input
         function (output) {
             // Output should be a list of numbers.
@@ -78,7 +68,7 @@ function AGTest(outputLog) {
                 actual;
             console.log(output);
 
-            expected = out;
+            expected = "12D687";
             if (output instanceof List) {
                 actual = output.asArray();
             } else {
@@ -102,20 +92,20 @@ function AGTest(outputLog) {
     );
 
 
-    var chunk_2 = fb.newChunk('Complete the "' + selection + '" block.');
+    var chunk_2 = fb.newChunk('Complete the "' + from_base + '" block.');
 
     var blockExists_2 = function () {
-        return spriteContainsBlock(selection);
+        return spriteContainsBlock(from_base);
     }
 
-    var tip_2_1 = chunk_2.newTip('Make sure you name your block exactly "' + selection + '" and place it in the scripting area.',
-        'The "' + selection + '" block exists and is recursive.');
+    var tip_2_1 = chunk_2.newTip('Make sure you name your block exactly "' + from_base + '" and place it in the scripting area.',
+        'The "' + from_base + '" block exists and is recursive.');
 
     tip_2_1.newAssertTest(
         blockExists_2,
-        'Testing if the "' + selection + '" block is in the scripting area.',
-        'The "' + selection + '" block is in the scripting area.',
-        'Make sure you name your block exactly "' + selection + '" and place it in the scripting area.',
+        'Testing if the "' + from_base + '" block is in the scripting area.',
+        'The "' + from_base + '" block is in the scripting area.',
+        'Make sure you name your block exactly "' + from_base + '" and place it in the scripting area.',
         1
     );
 
@@ -124,9 +114,9 @@ function AGTest(outputLog) {
         'Great job! Your block reports the correct value for given inputs.'
     );
 
-    var input_2_2_1 = [input];
+    var input_2_2_1 = ["12D687", "16"];
     tip_2_2.newIOTest('r',  // testClass
-        selection,          // blockSpec
+        from_base,          // blockSpec
         input_2_2_1,        // input
         function (output) {
             // Output should be a list of numbers.
@@ -134,7 +124,7 @@ function AGTest(outputLog) {
                 actual;
             console.log(output);
 
-            expected = out;
+            expected = "1234567";
             if (output instanceof List) {
                 actual = output.asArray();
             } else {
@@ -148,61 +138,6 @@ function AGTest(outputLog) {
             if (!_.isEqual(actual, expected)) {
                 tip_2_2.suggestion = 'The output should be ' + expected + ';';
                 tip_2_2.suggestion += ' but was ' + actual + '.';
-                return false;
-            }
-            return true;
-        },
-        4 * 1000, // 4 second time out.
-        true, // is isolated
-        1 // points
-    );
-
-    var chunk_3 = fb.newChunk('Complete the "' + partition + '" block.');
-
-    var blockExists_3 = function () {
-        return spriteContainsBlock(partition);
-    }
-
-    var tip_3_1 = chunk_3.newTip('Make sure you name your block exactly "' + partition + '", place it in the scripting area, and that it is recursive.',
-        'The "' + partition + '" block exists and is recursive.');
-
-    tip_3_1.newAssertTest(
-        blockExists_3,
-        'Testing if the "' + partition + '" block is in the scripting area.',
-        'The "' + partition + '" block is in the scripting area.',
-        'Make sure you name your block exactly "' + partition + '" and place it in the scripting area.',
-        1
-    );
-
-    var tip_3_2 = chunk_3.newTip(
-        'Your block should return the correct values for the given inputs.',
-        'Great job! Your block reports the correct value for given inputs.'
-    );
-
-    var input_3_2_1 = [input];
-    tip_3_2.newIOTest('r',  // testClass
-        partition,          // blockSpec
-        input_3_2_1,        // input
-        function (output) {
-            // Output should be a list of numbers.
-            var expected,
-                actual;
-            console.log(output);
-
-            expected = out;
-            if (output instanceof List) {
-                actual = output.asArray();
-            } else {
-                actual = output;
-                actual += ""; //to string
-            }
-            for (i = 0; i < actual.length; i++)
-            {
-                actual[i] = actual[i] + ""; //turns into strings
-            }
-            if (!_.isEqual(actual, expected)) {
-                tip_1_2.suggestion = 'The output should be ' + expected + ';';
-                tip_1_2.suggestion += ' but was ' + actual + '.';
                 return false;
             }
             return true;
