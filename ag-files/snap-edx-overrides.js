d2030d9deefa3c0b97d14db27c0e11b0b2d6c9fd
Michael Ball <cycomachead@gmail.com>
2017-01-12 21:27:21 -0800

    Fix for loading tools..how long has this been broken....

--- ag-files/snap-edx-overrides.js
+++ ag-files/snap-edx-overrides.js
@@ -3,7 +3,8 @@
       
 // edX doesn't support folders and instead uses _
 IDE_Morph.prototype.resourceURL = function (folder, file) {
-  return folder + '_' + file;
+  var args = Array.prototype.slice.call(arguments, 0);
+  return args.join('_');
 }
 
 
