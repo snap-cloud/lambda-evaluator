314a491e6b5955788501b50560eadc02d8fdf51f
Mansi Shah <emansishah@berkeley.edu>
2017-03-03 11:28:29 -0800

    added integer test

---
+++ edc/unit2/U2_W4_L3_T1.js
@@ -0,0 +1,240 @@
+var starter_path = null;
+// The id is to act as a course identifier.
+// NOTE: FOR NOW YOU ALSO HAVE TO ADD THE ID TO THE BOTTOM OF THE PAGE.
+var courseID = "edc";  // e.g. "BJCx"
+// Specify a prerequisite task id, should be null if no such requirement.
+var preReqTaskID = null;
+var preReqID = courseID + preReqTaskID;
+// taskID uniquely identifies the task for saving in browser sessionStorage.
+var taskID = "_U2_W4_L3_T1_E4";
+var id = courseID + taskID;
+var isEDX = isEDXurl();
+// if this question is not meant to be graded, change this flag to false
+var graded = true;
+// to hide feedback for this problem, set this to false
+var showFeedback = true;
+// to allow ability to regrade certain tests, set this to true
+var regradeOn = true;
+function AGTest(outputLog) {
+    var fb = new FeedbackLog(
+        world,
+        id,
+        'Script Variables'
+    );
+
+    var blockName = "integer? %";
+
+    /*var spriteIndex;
+    var ide = world.children[0];
+    var sprites = ide.sprites.contents;
+    for (var i = 0; i < sprites.length; i++) {
+        if (sprites[i].name === "Minimize Function") {
+            spriteIndex = i;
+            break;
+        }
+    }*/
+
+    var chunk_1 = fb.newChunk('Complete the "' + blockName + '" block.');
+
+    var blockExists_1 = function () {
+        return spriteContainsBlock(blockName);
+    }
+
+
+    var tip_1_1 = chunk_1.newTip('Make sure you name your block exactly "' + blockName + '" and place it in the scripting area.',
+        'The "' + blockName + '" block exists and uses the suggested blocks.');
+
+    tip_1_1.newAssertTest(
+        blockExists_1,
+        'Testing if the "' + blockName + '" block is in the scripting area.',
+        'The "' + blockName + '" block is in the scripting area.',
+        'Make sure you name your block exactly "' + blockName + '" and place it in the scripting area.',
+        1
+    );
+
+    var tip_1_2 = chunk_1.newTip(
+        'Your block should return the correct values for the given inputs.',
+        'Great job! Your block reports the correct value for given inputs.'
+    );
+
+    var tip_1_3 = chunk_1.newTip('Make sure your block definition uses the round block.')
+
+    var blockContains_round = function() {
+      return customBlockContains('integer? %n', 'round %n')
+    }
+
+    tip_1_3.newAssertTest(
+      blockContains_round,
+      'Testing if the ' + blockName + 'definition contains the round block.',
+      'The ' + blockName + 'block uses round in its solution.',
+      'Make sure you use the round block in your solution.',
+      1
+    )
+
+    var input_1_2_1 = ["10"];
+    tip_1_2.newIOTest('r',  // testClass
+        blockName,          // blockSpec
+        input_1_2_1,        // input
+        function (output) {
+            // Output should be a list of numbers.
+            var expected,
+                actual;
+            console.log(output);
+
+            expected = "true";
+            if (output instanceof List) {
+                actual = output.asArray();
+            } else {
+                actual = output;
+                actual += ""; //to string
+            }
+            for (i = 0; i < actual.length; i++)
+            {
+                actual[i] = actual[i] + ""; //turns into strings
+            }
+            if (!_.isEqual(actual, expected)) {
+                tip_1_2.suggestion = 'The output should be ' + expected + ';';
+                tip_1_2.suggestion += ' but was ' + actual + '.';
+                return false;
+            }
+            return true;
+        },
+        4 * 1000, // 4 second time out.
+        true, // is isolated
+        1 // points
+    );
+
+    var input_1_2_3 = ["0"];
+    tip_1_2.newIOTest('r',  // testClass
+        blockName,          // blockSpec
+        input_1_2_3,        // input
+        function (output) {
+            // Output should be a list of numbers.
+            var expected,
+                actual;
+            console.log(output);
+
+            expected = "true";
+            if (output instanceof List) {
+                actual = output.asArray();
+            } else {
+                actual = output;
+                actual += ""; //to string
+            }
+            for (i = 0; i < actual.length; i++)
+            {
+                actual[i] = actual[i] + ""; //turns into strings
+            }
+            if (!_.isEqual(actual, expected)) {
+                tip_1_2.suggestion = 'The output should be ' + expected + ';';
+                tip_1_2.suggestion += ' but was ' + actual + '.';
+                return false;
+            }
+            return true;
+        },
+        4 * 1000, // 4 second time out.
+        true, // is isolated
+        1 // points
+    );
+
+    var input_1_2_4 = ["-100"];
+    tip_1_2.newIOTest('r',  // testClass
+        blockName,          // blockSpec
+        input_1_2_4,        // input
+        function (output) {
+            // Output should be a list of numbers.
+            var expected,
+                actual;
+            console.log(output);
+
+            expected = "true";
+            if (output instanceof List) {
+                actual = output.asArray();
+            } else {
+                actual = output;
+                actual += ""; //to string
+            }
+            for (i = 0; i < actual.length; i++)
+            {
+                actual[i] = actual[i] + ""; //turns into strings
+            }
+            if (!_.isEqual(actual, expected)) {
+                tip_1_2.suggestion = 'The output should be ' + expected + ';';
+                tip_1_2.suggestion += ' but was ' + actual + '.';
+                return false;
+            }
+            return true;
+        },
+        4 * 1000, // 4 second time out.
+        true, // is isolated
+        1 // points
+    );
+
+    tip_1_2.newIOTest('r',  // testClass
+        blockName,          // blockSpec
+        ["4.5"],        // input
+        function (output) {
+            // Output should be a list of numbers.
+            var expected,
+                actual;
+            console.log(output);
+
+            expected = "false";
+            if (output instanceof List) {
+                actual = output.asArray();
+            } else {
+                actual = output;
+                actual += ""; //to string
+            }
+            for (i = 0; i < actual.length; i++)
+            {
+                actual[i] = actual[i] + ""; //turns into strings
+            }
+            if (!_.isEqual(actual, expected)) {
+                tip_1_2.suggestion = 'The output should be ' + expected + ';';
+                tip_1_2.suggestion += ' but was ' + actual + '.';
+                return false;
+            }
+            return true;
+        },
+        4 * 1000, // 4 second time out.
+        true, // is isolated
+        1 // points
+    );
+
+    tip_1_2.newIOTest('r',  // testClass
+        blockName,          // blockSpec
+        ["0.5"],        // input
+        function (output) {
+            // Output should be a list of numbers.
+            var expected,
+                actual;
+            console.log(output);
+
+            expected = "false";
+            if (output instanceof List) {
+                actual = output.asArray();
+            } else {
+                actual = output;
+                actual += ""; //to string
+            }
+            for (i = 0; i < actual.length; i++)
+            {
+                actual[i] = actual[i] + ""; //turns into strings
+            }
+            if (!_.isEqual(actual, expected)) {
+                tip_1_2.suggestion = 'The output should be ' + expected + ';';
+                tip_1_2.suggestion += ' but was ' + actual + '.';
+                return false;
+            }
+            return true;
+        },
+        4 * 1000, // 4 second time out.
+        true, // is isolated
+        1 // points
+    );
+
+
+    return fb;
+
+    }
