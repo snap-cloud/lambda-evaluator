630143cca642797908f373cf630b5ab95682076a
Michael Ball <cycomachead@gmail.com>
2017-08-26 21:51:08 -0700

    Scaffold thoughts on a better API

---
+++ ag-files/Autograder.js
@@ -0,0 +1,12 @@
+/*
+    WIP - A new entrypoint for the Autograder.
+*/
+
+
+var SnapAutograder = function (world, config) {
+    this.world = world;
+}
+
+SnapAutograder.prototype.registerTests = function () {
+    
+}
